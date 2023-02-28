1dbdfb303901d8cf98de4cb32a2bb0104c48fef0
Aerisfan01 <19flaszatommy@gmail.com>
2023-02-28 19:06:00 +1100

    Update STAR WALKINNN.py

--- STAR WALKINNN.py
+++ STAR WALKINNN.py
@@ -1,23 +1,25 @@
 print("Let's start the walk-a-thon!")
-participants = {}  # dictionary to store participant names and their distances walked
-total_distance = 0  # variable to keep track of the total distance walked
+participants = []
+total_distance = 0
 
 while True:
     name = input("Who has recorded a walk? ")
-    if name == "":
-        break  # exit the loop if no name is entered
-
+    if not name:
+        break
     distance = float(input("How far did they walk? "))
-    total_distance += distance  # update the total distance walked
-
-    if name in participants:
-        participants[name] += distance  # add distance walked to existing participant
-        print(f"Another walk from {name}. Well done on another {participants[name]:.1f} km!")
-    else:
-        participants[name] = distance  # add new participant
-        print(f"That's a first time for {name}! They walked {distance:.1f} km!")
-
-print(f"Thanks for taking part in the walk-a-thon! We walked a total of {total_distance:.2f} km!")
+    total_distance += distance
+    found = False
+    for participant in participants:
+        if participant['name'] == name:
+            participant['distance'] += distance
+            print(f"Another walk from {name}. Well done on another {distance} km!")
+            found = True
+            break
+    if not found:
+        participants.append({'name': name, 'distance': distance})
+        print(f"That's a first time for {name}! They walked {distance} km!")
+        
+print(f"Thanks for taking part in the walk-a-thon! We walked a total of {total_distance} km!")
 print("Here is a list of merit certificate winners:")
-for name in sorted(participants):
-    print("🏅", name)
+for participant in sorted(participants, key=lambda x: x['name']):
+    print("🏅", participant['name'])
